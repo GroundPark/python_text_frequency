29e682e92d7e94aa97f5a8718e3eaad390686f42
GroundPark <38882491+GroundPark@users.noreply.github.com>
2020-07-13 13:04:40 +0900

    Initial commit

---
+++ text_frequency.py
@@ -0,0 +1,18 @@
+﻿print("----------------------------------------------")
+txt = input('     ')
+txt2 = list(txt)
+print("----------------------------------------------")
+print("문자개수 :", len(txt))
+
+def freq(txt2):
+    Dic={}
+    for i in txt2:
+        Cnt=txt2.count(i);
+        Dic[i]=Cnt
+    for cnts in Dic:
+        print(cnts,":",Dic[cnts])
+    return
+
+print("----------------------------------------------")
+freq(txt2)
+print("----------------------------------------------")
